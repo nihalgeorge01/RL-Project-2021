d47dc362f932911ef148327c56e3b0f97071941f
Nihal John George <nihalgeorge01@gmail.com>
2021-11-26 10:15:21 +0530

    Add energy reward and beta decay

--- agent.py
+++ agent.py
@@ -34,6 +34,7 @@ class Agent:
 
         self.beta = self.config[2]
         self.alpha = self.config[3]
+        self.beta_inv = int(1/self.beta)
         self.eps = 1
         self.eps_decay = 0.99
         pass
@@ -61,6 +62,53 @@ class Agent:
         #raise NotImplementedError
         #return action
 
+    def acro_reward(self, obs):
+        m = 1.0
+        g = 9.8
+        L = 1.0
+        Icm = m*L*L/12
+        Iend = Icm*4
+        Lb2 = L/2
+        pi = 3.14159265
+
+        # Hyperparams: relative importance of each variable
+        pe_scale = 5
+        ke_scale = 5
+        ke1_scale = 1
+        ke2_scale = 0.2
+        te_scale = 5
+
+        # c = cos(), s = sin()
+        c1 = obs[0,0]
+        s1 = obs[0,1]
+        c2 = obs[0,2]
+        s2 = obs[0,3]
+        c12 = c1*c2 - s1*s2     #cos(theta1+theta2)
+        s12 = s1*c2 + c1*s2     
+        v1 = obs[0,4]
+        v2 = obs[0,5]
+        v1_max = 4*pi
+        v2_max = 9*pi
+
+        # Potential energy
+        pe1 = -m*g*(Lb2)*c1
+        pe2 = -m*g*(L*c1 + Lb2*c12)
+        pe0 = 2*m*g*L
+        pe = (pe1+pe2 - pe0)/pe0
+        
+        # Kinetic Energy
+        ke1 = 0.5*Iend*v1*v1
+        ke1_max = 0.5*Iend*v1_max*v1_max
+        ke2 = 0.5*(m*L*L + Iend + 2*m*L*Lb2*c1)*v1*v1 + 0.5*Iend*v2*v2 + (Iend + m*L*Lb2*c2)*v1*v2
+        ke2_max = 0.5*(m*L*L + Iend + 2*m*L*Lb2*1.0)*v1_max*v1_max + 0.5*Iend*v2_max*v2_max + (Iend + m*L*Lb2*1.0)*v1_max*v2_max
+        ke_max = ke1_scale*ke1_max + ke2_scale*ke2_max
+        ke = ((ke1_scale*ke1 + ke2_scale*ke2) - ke_max)/ke_max
+
+        te_max = pe0 + ke_max
+        te = te_scale *(ke_scale*ke + pe_scale*pe - te_max)/te_max
+
+        return te
+
     def compute_action_train(self, obs, reward, done, info):
         """
         Use this function in the train phase
@@ -77,8 +125,8 @@ class Agent:
         if self.env_name == 'acrobot':
             obs = np.array(obs).reshape(1,-1)
             q_val_prev = np.matmul(self.obs_prev, self.weights[:,self.action].reshape(-1,1))[0]
-
-            error = self.obs_prev*(reward + (1-done)*self.alpha*np.max(np.matmul(obs, self.weights)) - q_val_prev)
+            a_reward = self.acro_reward(obs)
+            error = self.obs_prev*(a_reward + (1-done)*self.alpha*np.max(np.matmul(obs, self.weights)) - q_val_prev)
             self.weights[:, self.action] += self.beta*error[0]
 
             q_vals = np.matmul(obs, self.weights)
@@ -96,6 +144,10 @@ class Agent:
 
         self.eps = max(0.1, self.eps*self.eps_decay)
         self.obs_prev = obs
+        
+        # # beta decay
+        # self.beta_inv += 0.0001
+        # self.beta = max(0.0001, 1.0/self.beta_inv)
 
         return self.action
         #raise NotImplementedError
--- run.py
+++ run.py
@@ -38,7 +38,8 @@ if __name__ == "__main__":
 
     N_TRAIN_EPISODES = {"acrobot": 2000, "taxi": 1500, "kbca": 2000, "kbcb": 2000, "kbcc": 2000}
 
-    N_EVAL_EPISODES = 100
+    # TODO : Change this back to 100
+    N_EVAL_EPISODES = 1000
 
     if ENV_NAME == "acrobot":
         env = aicrowd_gym.make("Acrobot-v1")
