7a60b4aa98e1a348e9462a3acdee7360a9329eb5
Nihal John George <nihalgeorge01@gmail.com>
2021-11-28 16:47:36 +0530

    Finish KBC code

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import argmax
 from config import *
 import time
 import numpy as np
@@ -50,11 +51,11 @@ class Agent:
         self.kbc_n = self.config[0]
         self.kbc_pe_ct = [0 for i in range(self.kbc_n)]
         self.kbc_ph_ct = [0 for i in range(self.kbc_n)]
-
+        self.kbc_train_ep = 0
+        self.kbc_pe_ep = 0
+        self.kbc_ph_ep = 0
+        self.kbc_test_ep = 0
         self.kbc_easy = True
-        
-
-        pass
 
     def register_reset_train(self, obs):
         """
@@ -71,6 +72,7 @@ class Agent:
             self.action = np.argmax(q_vals)
         
         elif self.env_name[:3] == 'kbc':
+            self.kbc_train_ep += 1
             self.action = 1
             self.kbc_step = 0
             if self.env_name[3] == 'c' and not self.kbc_easy:
@@ -192,31 +194,38 @@ class Agent:
             else:       # Just store counts of how many times a state was reached
                 if self.env_name[3] == 'a': # KBC A - Finding pe, ge - Simulate as many paths as possible, find empirical probabs
                     if not done:    # Keep attempting questions
+                        self.kbc_pe_ct[self.kbc_step-1] += 1 # Increment counter of correct attempts
                         self.action = 1
                     else:   # We lost at Q_step-1
-                        self.kbc_pe_ct[self.kbc_step-1] += 1
                         self.action = 1
+                        self.kbc_pe_ep += 1 # Increment episode count
                 
                 elif self.env_name[3] == 'b':   # KBC B - Finding pe, ge, kc - Simulate as many paths as possible, never pulling out, find empirical probabs.
                     if not done:    # Keep attempting questions
+                        self.kbc_pe_ct[self.kbc_step-1] += 1 # Increment counter of correct attempts
                         self.action = 1
                     else:   # We lost at Q_step-1
-                        self.kbc_pe_ct[self.kbc_step-1] += 1
                         self.action = 1
-                        if reward != 0:     # We passed a checkpoint somewhere, note reward = r0*b^(kc-1), Use maths to get kc
-                            self.kbc_kc = np.log(float(reward)/self.kbc_r0)/np.log(self.kbc_b) + 1
+                        self.kbc_pe_ep += 1
+                        if reward != 0:     # We crossed a checkpoint somewhere, note reward = r0*b^(kc-1), Use maths to get kc
+                            self.kbc_kc = int(np.log(float(reward)/self.kbc_r0)/np.log(self.kbc_b)) + 1
                 
                 else:   # KBC C -Finding pe, ph, ge, gh - Simulate as many paths as possible, find empirical probabs. Alternate between episodes of all easy and all hard questions
-                    if done:   # We lost at Q_step-1
+                    if not done:   # Don't change self.action, just do the same
                         if self.kbc_easy:
                             self.kbc_pe_ct[self.kbc_step-1] += 1
+                        else:
+                            self.kbc_ph_ct[self.kbc_step-1] += 1
+                    else:   # We lost at Q_step-1, switch to other question mode
+                        if self.kbc_easy:
+                            self.kbc_pe_ep += 1
                             self.kbc_easy = False
                             self.action = 2
                         else:
-                            self.kbc_ph_ct[self.kbc_step-1] += 1
+                            self.kbc_ph_ep += 1
                             self.kbc_easy = True
                             self.action = 1
-                    # if not done, don't change self.action, just do the same
+                    
         
         elif self.env_name == 'taxi':
             self.q_table[self.obs_prev][self.action] += self.beta*(reward + (1-done)*self.alpha*np.max(self.q_table[obs]) - \
@@ -253,14 +262,68 @@ class Agent:
         
         elif self.env_name[:3] == 'kbc':
             # If first test episode, set variables and DP tables
+            if self.kbc_test_ep == 0:
                 # If any params are unset (at -1), put some default value
-
+                if self.kbc_r0 == -1:
+                    self.kbc_r0 = 1000
+                if self.kbc_b == -1:
+                    self.kbc_b = 2
+                # kbc_kc is already set correctly to 0 if not KBC B
+                
                 # Build rewards and probab arrays
+                self.kbc_rewards = [0] + [self.kbc_r0 * (self.kbc_b ** i) for i in range(self.kbc_n)]
+                self.kbc_rewards[-1] = self.kbc_r0 * (self.kbc_b ** (self.kbc_n-2)) # Env has a mistake, setter's side issue
+                # TODO : Least squares based estimate of px, gx
+                # self.kbc_pe_log_emp = []
+                # for val in self.kbc_pe_ct:
+                #     if val>0:
+                #         self.kbc_pe_log_emp.append(np.log(val/self.kbc_pe_ep))
+                #     else:
+                #         break
+                
+                # # Least square fit to get ln(ge) and ln(pe) + 0.5ln(ge)
 
+                # # Finding hard q params for KBC C
+                # if self.env_name[3] == 'c':
+                #     self.kbc_ph_log_emp = []
+                #     for val in self.kbc_ph_ct:
+                #         if val>0:
+                #             self.kbc_ph_log_emp.append(np.log(val/self.kbc_ph_ep))
+                #         else:
+                #             break
+                #     # Least square fit to get ln(gh) and ln(ph) + 0.5ln(gh)
+                # END TODO
+
+                self.kbc_pe = self.kbc_pe_ct[0]/self.kbc_pe_ep
+                self.kbc_ge = (self.kbc_pe_ct[1]/self.kbc_pe_ep)/(self.kbc_pe ** 2)
+                self.kbc_pe_ns_probs = [self.kbc_pe * (self.kbc_ge ** i) for i in range(self.kbc_n)]
+
+                if self.env_name[3] == 'c':
+                    self.kbc_ph = self.kbc_ph_ct[0]/self.kbc_ph_ep
+                    self.kbc_gh = (self.kbc_ph_ct[1]/self.kbc_ph_ep)/(self.kbc_ph ** 2)
+                    self.kbc_ph_ns_probs = [self.kbc_ph * (self.kbc_gh ** i) for i in range(self.kbc_n)]
+                
                 # Build DP table, get optimal policy
+                self.kbc_dp = [-1 for i in range(self.kbc_n)]
+                self.kbc_pol = [0 for i in range(self.kbc_n)]
+
+                self.kbc_dp[-1] = self.kbc_rewards[-1]
+                self.kbc_pol[-1] = 0
+
+                for i in range(self.kbc_n-2, -1, -1):
+                    if self.env_name[3] == 'c':
+                        choices = [self.kbc_rewards[i], self.kbc_pe_ns_probs[i]*self.kbc_dp[i+1], self.kbc_ph_ns_probs[i]*self.kbc_dp[i+1] + (1-self.kbc_ph_ns_probs[i])*(self.kbc_rewards[i]/2.0)]
+                    elif i > self.kbc_kc:
+                        choices = [self.kbc_rewards[i], self.kbc_pe_ns_probs[i]*self.kbc_dp[i+1] + (1-self.kbc_pe_ns_probs[i])*self.kbc_rewards[self.kbc_kc]]
+                    else:
+                        choices = [self.kbc_rewards[i], self.kbc_pe_ns_probs[i]*self.kbc_dp[i+1]]
+                    
+                    self.kbc_dp[i] = max(choices)
+                    self.kbc_pol[i] = argmax(choices)
 
             # Take optimal action given obs and DP table
-            pass
+            self.kbc_step = 0
+            action = self.kbc_pol[0]
         
         elif self.env_name == 'taxi':
             action = np.argmax(self.q_table[obs])
@@ -287,7 +350,12 @@ class Agent:
         
         elif self.env_name[:3] == 'kbc':
             # Use previously computed optimal policy
-            pass
+            self.kbc_step += 1
+            if done:
+                self.kbc_test_ep += 1
+                action = 0
+            else:
+                action = self.kbc_pol[self.kbc_step]
         
         elif self.env_name == 'taxi':
             action = np.argmax(self.q_table[obs])
